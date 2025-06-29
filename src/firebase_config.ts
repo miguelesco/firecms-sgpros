2cffbf59ab8a99fabb270f053fcd3bf5c7a65125
miguelesco <miguelangelescorche@gmail.com>
2025-06-29 12:39:09 -0500

    Initial commit - FireCMS setup

---
+++ src/firebase_config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    "projectId": "sgpros",
+    "appId": "1:615728099354:web:38acd3a4c4443e4a3c61a3",
+    "storageBucket": "sgpros.firebasestorage.app",
+    "locationId": "us-central",
+    "apiKey": "AIzaSyAGpKxx6-8T6jR5FZzsk4lqrPIskZ07XC0",
+    "authDomain": "sgpros.firebaseapp.com",
+    "messagingSenderId": "615728099354",
+    "measurementId": "G-KX7ZJ7FPB7"
+}
